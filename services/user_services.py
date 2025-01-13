45377bcef1f74e65ee01937ef055bbd62c341514
Mritunjay Paswan <mritunjay@manthanitsolutions.in>
2025-01-13 14:49:51 +0530

    - test

--- services/user_services.py
+++ services/user_services.py
@@ -2,6 +2,6 @@ import bcrypt
 
 
 def hash_password(password):
-    salt = bcrypt.gensalt()
+    salt = bcrypt.gensalt() 
     hashed_pass = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_pass.decode('utf-8')
